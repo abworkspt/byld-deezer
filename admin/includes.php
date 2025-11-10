8df39fbc4c10390690935c1add9323701070822c
Andre Bernardes <andreb07@gmail.com>
2025-11-10 16:04:04 +0000

    Update

--- admin/includes.php
+++ admin/includes.php
@@ -19,7 +19,7 @@ function include_scripts_styles() {
 		wp_dequeue_style( 'wp-admin' );
 				
 		// TEMPLATE
-		wp_enqueue_style( 'main',  get_template_directory_uri(). '/css/main.min.css',false,'1.8','all');
+		wp_enqueue_style( 'main',  get_template_directory_uri(). '/css/main.min.css',false,'1.9','all');
     }
 }
 
